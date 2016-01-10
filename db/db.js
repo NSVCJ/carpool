b864217b9adde6a89fb1324b54f664a7daab0722
Niicck <nickivons@yahoo.com>
2016-01-09 17:31:47 -0800

    (feature) create user, trip, and tripUser tables

--- db/db.js
+++ db/db.js
@@ -1,6 +1,21 @@
 
-var sequelize = require("sequelize")
+var Sequelize = require("sequelize")
 
-var seq = new sequilize("carpool","admin");
+var sequelize = new Sequelize("carpool", "root", "");
 
-var User = 
+var User = sequelize.define("User", {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+  phone: Sequelize.STRING,
+  profilePicture: Sequelize.BLOB('long'),
+  rating: Sequelize.FLOAT(4),
+  numberOfRatings: Sequelize.INTEGER
+});
+
+var Trip = sequelize.define("Trip", {
+  startTime: Sequelize.DATE(),
+  price: Sequelize.DECIMAL(7,2)
+})
+
+Trip.belongsToMany(User, {through: 'TripUsers'});
+User.belongsToMany(Trip, {through: 'TripUsers'});
