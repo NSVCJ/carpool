f3ee2739a1c6293dc69bd386df6ac28523cd639e
Niicck <nickivons@yahoo.com>
2016-01-11 14:21:58 -0800

    (refactor) removed unneeded db tables

--- server/db/db.js
+++ server/db/db.js
@@ -6,36 +6,37 @@ var User = orm.define("User", {
   name: Sequelize.STRING,
   email: Sequelize.STRING,
   phone: Sequelize.STRING,
-  profile_picture: Sequelize.BLOB('long'),
+  profilePicture: Sequelize.BLOB('long'),
   rating: Sequelize.FLOAT(4),
-  ratings_count: Sequelize.INTEGER
+  ratingsCount: Sequelize.INTEGER
 });
 
 var Trip = orm.define("Trip", {
-  start_time: Sequelize.DATE(),
-  price: Sequelize.DECIMAL(7,2)
+  startTime: Sequelize.DATE(),
+  price: Sequelize.DECIMAL(7,2),
+  eventfulId: Sequelize.STRING
 });
 
-var Event = orm.define("Event", {
-  eventful_id: Sequelize.STRING,
-  name: Sequelize.STRING,
-  location_lat: Sequelize.DECIMAL(20,18),
-  location_long: Sequelize.DECIMAL(20,17),
-  //Examine the api to know for sure
-  start_time: Sequelize.DATE(),
-  type: Sequelize.STRING,
-  description: Sequelize.STRING
-  //URL to more info?
-  //URL to tickets?
-});
+// var Event = orm.define("Event", {
+//   eventful_id: Sequelize.STRING,
+//   name: Sequelize.STRING,
+//   location_lat: Sequelize.DECIMAL(20,18),
+//   location_long: Sequelize.DECIMAL(20,17),
+//   //Examine the api to know for sure
+//   start_time: Sequelize.DATE(),
+//   type: Sequelize.STRING,
+//   description: Sequelize.STRING
+//   //URL to more info?
+//   //URL to tickets?
+// });
 
 var TripUser = orm.define("TripUser", {
-  start_lat: Sequelize.DECIMAL(20,18),
-  start_long: Sequelize.DECIMAL(20,17),
+  lat: Sequelize.DECIMAL(20,18),
+  long: Sequelize.DECIMAL(20,17),
   role: Sequelize.STRING
 });
 
-Event.hasMany(Trip);
+//Event.hasMany(Trip);
 
 Trip.belongsToMany(User, {through: 'TripUser'});
 User.belongsToMany(Trip, {through: 'TripUser'});
@@ -57,13 +58,13 @@ User.belongsToMany(Trip, {through: 'TripUser'});
 
 User.sync();
 Trip.sync();
-Event.sync();
+//Event.sync();
 TripUser.sync();
 //EventUser.sync();
 
 exports.User = User;
 exports.Message = Trip;
-exports.Event = Event;
+//exports.Event = Event;
 exports.TripUser = TripUser;
 //exports.EventUser = EventUser;
 exports.orm = orm;
--- server/models/index.js
+++ server/models/index.js
@@ -17,46 +17,37 @@ module.exports = models = {
       //   callback(data);
       // }
     },
-    post: function(cb, data) {
-      /*
+    post: function(callback, data) {
+
       //MVP: no profile associated, so a new user is created for every post.
       //data must pass in info about driver, event, and trip
       //maybe data should be an object with each of those properties
       db.User.create( {
-        name: data.name,
-        email: data.email,
-        phone: data.phone
+        name: data.user.name,
+        email: data.user.email,
+        phone: data.user.phone
       }).then(function(user) {
-        cb(user)
-        //What if the event exists in db already?
-        if (serverHelpers.eventExists) {
-        }
-        //or do .then? Maybe. So you can pass on the object?
-        db.Event.create( {
-          name: data.event.name,
-          location_lat: data.event.lat,
-          location_long: data.event.long,
-          start_time: data.event.startTime,
-          type: data.event.type,
-          description: data.event.description
-        }).then(function(event) {
-          db.Trip.create( {
-            start_time: data.startTime,
-            price: data.price
-          }).then(function(trip) {
-            db.TripUser.create( {
-              start_lat: data.startLat,
-              start_long: data.startLong,
-              role: "Driver"
-            }).then(function(tripUser) {
-              console.log(tripUser); //see if I have setter functions
-              //Set user
-              //Set event
-            })
-          })
-        })
-      })
-      */
+        db.Trip.create( {
+          start_time: data.trip.startTime,
+          price: data.trip.price,
+          eventful_id: data.event.id
+        }).then(function(trip) {
+          db.TripUser.create( {
+            lat: data.trip.lat,
+            long: data.trip.long,
+            role: "Driver"
+          }).then(function(tripUser) {
+            console.log(tripUser); //see if I have setter functions
+            tripUser.setUser(user); //not sure if setUser is a methods here
+            tripUser.setTrip(trip);
+            callback({
+              user: user,
+              trip: trip,
+              tripUser: tripUser
+            });
+          });
+        });
+      });
     }
   }
 }
