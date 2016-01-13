1159d7c5362647f45b35edebf9e45c2c393227f6
Niicck <nickivons@yahoo.com>
2016-01-13 11:18:17 -0800

    (refactor) Created new models for MVP+

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -20,21 +20,32 @@ module.exports = {
     get: function (req, res) {
       console.log('inside controllers trips get');
       models.trips.get(function(data){
-        //console.log("Inside trips get", data);
         res.send({trips: data});
       }, req.query)
     },
     post: function (req, res) {
+      console.log('inside controllers trips post')
       models.trips.post(function(data){
-        //console.log('inside controllers trips post')
         res.send({posted: data});
       }, req.body); //some function to get data, fix later
     }
   },
-  riderProfile:{
+  riderConfirmed:{
     get: function (req, res) {
-      console.log('inside controllers trips riders');
-      models.trips.riders(function(riderInfo, driverInfo){
+      console.log('inside controllers riderConfirmed get');
+      models.riderConfirmed.get(function(riderInfo, driverInfo){
+        //console.log("Inside trips get", data);
+        res.send({riderInfo: riderInfo,
+                  driverInfo: driverInfo
+        });
+      }, req.query)
+    },
+    post: function (req, res) {}
+  },
+  riderUnconfirmed:{
+    get: function (req, res) {
+      console.log('inside controllers riderUnconfirmed get');
+      models.riderUnconfirmed.get(function(riderInfo, driverInfo){
         //console.log("Inside trips get", data);
         res.send({riderInfo: riderInfo,
                   driverInfo: driverInfo
@@ -57,6 +68,59 @@ module.exports = {
     },
     post: function(req, res){
 
+    }
+  },
+  driverConfirmed:{
+    get: function (req, res) {
+      console.log('inside controllers driverConfirmed get');
+      models.driverConfirmed.get(function(riderInfo, driverInfo){
+        //console.log("Inside trips get", data);
+        res.send({riderInfo: riderInfo,
+                  driverInfo: driverInfo
+        });
+      }, req.query)
+    },
+    post: function (req, res) {}
+  },
+  driverUnconfirmed:{
+    get: function (req, res) {
+      console.log('inside controllers driverUnconfirmed get');
+      models.driverUnconfirmed.get(function(riderInfo, driverInfo){
+        //console.log("Inside trips get", data);
+        res.send({riderInfo: riderInfo,
+                  driverInfo: driverInfo
+        });
+      }, req.query)
+    },
+    post: function (req, res) {}
+  },
+  eventRider: {
+    get: function (req, res) {
+      console.log('inside controllers eventRider get');
+      models.trips.get(function(data){
+        //console.log("Inside trips get", data);
+        res.send({trips: data});
+      }, req.query)
+    },
+    post: function (req, res) {
+      console.log('inside controllers eventRider post')
+      models.trips.post(function(data){
+        res.send({posted: data});
+      }, req.body); //some function to get data, fix later
+    }
+  },
+  eventDriver: {
+    get: function (req, res) {
+      console.log('inside controllers eventDriver');
+      models.trips.get(function(data){
+        res.send({trips: data});
+      }, req.query)
+    },
+    post: function (req, res) {
+      models.trips.post(function(data){
+        //console.log('inside controllers trips post')
+        res.send({posted: data});
+      }, req.body); //some function to get data, fix later
     }
   }
 };
