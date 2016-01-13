ff3fcc3fa16b930eca78f1a21ed769f70abee02f
Niicck <nickivons@yahoo.com>
2016-01-12 16:36:13 -0800

    (feature) begin rider profile queries

--- server/models/index.js
+++ server/models/index.js
@@ -19,6 +19,11 @@ module.exports = models = {
         callback(data);
       })
     },
+    riders: function(callback, params) {
+      db.sequelize.query(
+        "select TripUsers.TripId, TripUsers.lat, TripUsers.long, Trips.price, Trips.eventfulId from TripUsers, Trips where TripUsers.UserId"
+      )
+    },
     post: function(callback, data) {
       //console.log("Here is your post data", data);
       //MVP: no profile associated, so a new user is created for every post.
