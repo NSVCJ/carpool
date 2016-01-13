515d022a8bdd91aaa0190b0be4ee5e9e8a6ee150
Niicck <nickivons@yahoo.com>
2016-01-13 11:19:08 -0800

    (feature) begin routing for MVP+ models

--- server/models/index.js
+++ server/models/index.js
@@ -21,10 +21,10 @@ module.exports = models = {
     get: function(){},
     post: function(){}
   },
-  riderProfile: {
+  riderConfirmed: {
     get: function(callback, params) {
       db.sequelize.query(
-        "select TripUsers.TripId, TripUsers.lat, TripUsers.long, Trips.price, Trips.eventfulId from TripUsers, Trips where TripUsers.UserId = '"+params.UserId+"' AND TripUsers = 'Rider' AND TripUsers.TripId = Trips.id",
+        "select TripUsers.TripId, TripUsers.lat, TripUsers.long, Trips.price, Trips.eventfulId from TripUsers, Trips where TripUsers.UserId = '"+params.UserId+"' AND TripUsers.role = 'Rider' AND TripUsers.TripId = Trips.id",
       {type: db.sequelize.QueryTypes.SELECT})
       .spread(function(riderInfo){
         var queries = riderInfo.map(function(trip) {
@@ -37,24 +37,93 @@ module.exports = models = {
           callback(riderInfo, driverInfo);
         })
       })
-    }
+    },
+    post: function(){}
   },
-  trips: {
+
+  riderUnconfirmed: {
     get: function(callback, params) {
-      //console.log("Them params", params);
+      db.sequelize.query(
+        "select TripUsers.TripId, TripUsers.lat, TripUsers.long, Trips.price, Trips.eventfulId from TripUsers, Trips where TripUsers.UserId = '"+params.UserId+"' AND TripUsers.role = 'Unconfirmed' AND TripUsers.TripId = Trips.id",
+      {type: db.sequelize.QueryTypes.SELECT})
+      .spread(function(riderInfo){
+        var queries = riderInfo.map(function(trip) {
+          db.sequelize.query(
+            "select Users.*, TripUsers.lat, TripUsers.long from Users, TripUsers where TripUsers.TripId = '"+trip.TripId+"' AND TripUsers.role = 'Driver'",
+          {type: db.sequelize.QueryTypes.SELECT})
+        })
+        Promise.all(queries)
+        .then(function(driverInfo){
+          callback(riderInfo, driverInfo);
+        })
+      })
+    },
+    post: function(){}
+  },
+
+  driverConfirmed: {
+    get: function(callback, params) {
+      db.sequelize.query(
+        "select TripUsers.TripId, TripUsers.lat, TripUsers.long, Trips.price, Trips.eventfulId from TripUsers, Trips where TripUsers.UserId = '"+params.UserId+"' AND TripUsers.role = 'Driver' AND TripUsers.TripId = Trips.id",
+      {type: db.sequelize.QueryTypes.SELECT})
+      .spread(function(driverInfo){
+        var queries = riderInfo.map(function(trip) {
+          db.sequelize.query(
+            "select Users.*, TripUsers.lat, TripUsers.long from Users, TripUsers where TripUsers.TripId = '"+trip.TripId+"' AND TripUsers.role = 'Rider'",
+          {type: db.sequelize.QueryTypes.SELECT})
+        })
+        Promise.all(queries)
+        .then(function(riderInfo){
+          callback(riderInfo, driverInfo);
+        })
+      })
+    },
+    post: function(){}
+  },
+
+  driverUnconfirmed: {
+    get: function(callback, params) {
+      db.sequelize.query(
+        "select TripUsers.TripId, TripUsers.lat, TripUsers.long, Trips.price, Trips.eventfulId from TripUsers, Trips where TripUsers.UserId = '"+params.UserId+"' AND TripUsers.role = 'Driver' AND TripUsers.TripId = Trips.id",
+      {type: db.sequelize.QueryTypes.SELECT})
+      .spread(function(driverInfo){
+        var queries = riderInfo.map(function(trip) {
+          db.sequelize.query(
+            "select Users.*, TripUsers.lat, TripUsers.long from Users, TripUsers where TripUsers.TripId = '"+trip.TripId+"' AND TripUsers.role = 'Unconfirmed'",
+          {type: db.sequelize.QueryTypes.SELECT})
+        })
+        Promise.all(queries)
+        .then(function(riderInfo){
+          callback(riderInfo, driverInfo);
+        })
+      })
+    },
+    post: function(){}
+  },
+
+
+  eventRider: {
+    //Get all Drivers for an event
+    //For use on event-Rider page
+    get: function(callback, params) {
       db.sequelize.query(
         "select TripUsers.lat, TripUsers.long, Trips.price, Users.name, Users.email, Users.phone from Trips, TripUsers, Users where eventfulId = '"+params.eventfulId+"' AND TripUsers.TripId = Trips.id AND TripUsers.role = 'Driver' AND Users.id = TripUsers.UserId",
       {type: db.sequelize.QueryTypes.SELECT})
       .then(function(data){
-        console.log("Inside models.trips.get", data);
         callback(data);
       })
     },
     post: function(callback, data) {
-      //console.log("Here is your post data", data);
+      //To be continued once the rider form is completed
+    }
+  },
+
+  eventDriver: {
+    //Post that you want to be a driver for an event/
+    //For use on event-Driver page
+    get: function(callback, data) {},
+    post: function(callback, data) {
       //MVP: no profile associated, so a new user is created for every post.
-      //data must pass in info about driver, event, and trip
-      //maybe data should be an object with each of those properties
       db.User.create( {
         name: data.user.name,
         email: data.user.email,
@@ -71,16 +140,51 @@ module.exports = models = {
             UserId: user.id,
             TripId: trip.id
           }).then(function(tripUser) {
-            //console.log("TripUser:", tripUser); //see if I have setter functions
             callback({
               'user': user,
               'trip': trip,
               'tripUser': tripUser
             });
           });
-          // tripUser.setUsers([user]); //not sure if setUser is a methods here
-          // tripUser.setTrips([trip]);
-          // tripUser.save();
+        });
+      });
+    }
+  },
+  //Will be depreciated after MVP
+  trips: {
+    get: function(callback, params) {
+      db.sequelize.query(
+        "select TripUsers.lat, TripUsers.long, Trips.price, Users.name, Users.email, Users.phone from Trips, TripUsers, Users where eventfulId = '"+params.eventfulId+"' AND TripUsers.TripId = Trips.id AND TripUsers.role = 'Driver' AND Users.id = TripUsers.UserId",
+      {type: db.sequelize.QueryTypes.SELECT})
+      .then(function(data){
+        console.log("Inside models.trips.get", data);
+        callback(data);
+      })
+    },
+    post: function(callback, data) {
+      //MVP: no profile associated, so a new user is created for every post.
+      db.User.create( {
+        name: data.user.name,
+        email: data.user.email,
+        phone: data.user.phone
+      }).then(function(user) {
+        db.Trip.create( {
+          price: data.trip.price,
+          eventfulId: data.event.id
+        }).then(function(trip) {
+          tripUser = db.TripUser.create( {
+            lat: data.trip.lat,
+            long: data.trip.long,
+            role: "Driver",
+            UserId: user.id,
+            TripId: trip.id
+          }).then(function(tripUser) {
+            callback({
+              'user': user,
+              'trip': trip,
+              'tripUser': tripUser
+            });
+          });
         });
       });
     }
