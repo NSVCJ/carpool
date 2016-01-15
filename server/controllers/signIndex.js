30656b692b6b884e198d9d8ad13bab50f5648e86
Chris Kuh <kuh_chris@yahoo.com>
2016-01-14 16:31:27 -0800

    (fix) removed password from response

--- server/controllers/signIndex.js
+++ server/controllers/signIndex.js
@@ -1,5 +1,6 @@
 //will need to require some fold called modles
 var models = require('../models/signIndex.js');
+var jwt  = require('jwt-simple');
 
 module.exports = {
   signin:{
@@ -18,6 +19,25 @@ module.exports = {
       models.signup.post(function(data){
         res.json(data);
       },req.body)
+    }
+  },
+  checkAuth:{
+    get: function(req, res){},
+    post: function(req, res, next){
+      console.log('inside controller checkAuth post');
+      var token = req.headers['x-access-token'];
+      if(!token){
+        next(new Error('No token'));
+      }else{
+        var username = jwt.decode(token, 'secret');
+        models.checkAuth.post(function(found){
+          if(found){
+            res.status(200).send();
+          }else{
+            res.status(401).send();
+          }
+        }, username)
+      }
     }
   }
 };
